299dcdf874f896f96043f01ecee11d9d74deb89f
OGLyxvan <ton_email_github@gmail.com>
2026-05-11 06:59:18 -0400

    detecteur de phishing v1

--- PYTHON/lecon4.py
+++ PYTHON/lecon4.py
@@ -5,12 +5,30 @@ urls = [
     "https://github.com"
 ]
 count_suspect = 0
+mots_suspects = ["secure", "verify", "login", "password", "account"]
+nom_de_domaine = (".tk", ".xyz", ".ml")
+marques = ["paypal", "apple", "amazon", "microsoft", "paypal-secure", "apple-login"]
 
 for i in urls:
+    suspects = False
     if (len(i) > 30):
-        print(f"{i} -> URL SUSPECTE")
-        count_suspect = count_suspect + 1
+        suspects = True
+    for j in mots_suspects:
+        if j in i:
+            suspects = True
+            break
+    if suspects == True:
+        count_suspect += 1
+    if i.startswith("http://"):
+        suspects = True
+    if i.endswith(nom_de_domaine):
+        suspects = True
+    for k in marques:
+        if k in i:
+            suspects = True
+            break
+    if suspects:
+        print(f"{i} -> SUSPECT")
     else:
-        print(f"{i} -> URL NORMALE")
-        
-print(f"--- Résultat ---\n {count_suspect} URL(s) suspecte(s) trouvée(s) sur 4\n")    
+        print(f"{i} -> NORMALE")
+print(f"--- Résultat ---\n {count_suspect} URL(s) suspecte(s) trouvée(s) sur 4\n")
